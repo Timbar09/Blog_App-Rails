d5af8b5314088e43e94f7c63a6e541f66a7af76e
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-03 23:42:43 +0200

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :author_id, :title, :text, :comments_counter, :likes_counter, presence: true
+  validates :title, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   belongs_to :author, class_name: 'User'

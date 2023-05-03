5aa02d0f70597b1242fbe0f284c5682f545e5fc5
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-03 23:03:08 +0200

    Add validations for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  validates :author_id, :post_id, :text, presence: true
+  validates :author_id, :post_id, numericality: { only_integer: true, greater_than: 0 }
+
   belongs_to :author, class_name: 'User'
   belongs_to :post
   after_save :update_comments_counter

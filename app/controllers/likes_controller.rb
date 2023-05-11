36e7952e954b6551e8a2b6477e297af8e4b7b408
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-11 20:15:48 +0200

    Create likes_controller.rb

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+class LikesController < ApplicationController
+  def create
+    like = Like.new()
+    post = Post.find(params[:post_id])
+    user = User.find(params[:user_id])
+    like.post = post
+    like.author = current_user
+    
+    if like.save
+      redirect_to user_post_path(user, post), notice: 'Like created successfully'
+    else
+      redirect_to user_post_path(user, post), alert: like.errors.full_messages.join(', ')
+    end
+  end
+end
